3182559d0060c512f5a4a9c39c870dc6b07ea630
Jose Ignacio <joseignacio.salinas@lvaindices.com>
2019-09-05 17:08:29 -0400

    Se agrega la seccion contacto y footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 $(function(){
 
     $('[data-toggle="popover"]').popover();
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
     
   });
